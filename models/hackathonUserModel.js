29e80d07ffa7d949938d656ae13157bab6009581
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2020-09-04 22:13:21 +0200

    updated the models

--- models/hackathonUserModel.js
+++ models/hackathonUserModel.js
@@ -1,66 +1,66 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const hackathonUserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name'],
-    trim: true
+    required: [true, "Please tell us your name"],
+    trim: true,
   },
   email: {
     type: String,
-    required: [true, 'Please tell us your email'],
-    unique: [true, 'Your email is used, please register with another email'],
-    validate: [validator.isEmail, 'Please provide a vaild email']
+    required: [true, "Please tell us your email"],
+    unique: [true, "Your email is used, please register with another email"],
+    validate: [validator.isEmail, "Please provide a vaild email"],
   },
   nationalID: {
     type: Number,
     unique: true,
-    required: [true, 'Please tell us your national ID']
+    required: [true, "Please tell us your national ID"],
   },
   phone: {
-    type: Number
+    type: Number,
   },
   governorate: {
     type: String,
-    required: [true, 'Please provide your residence governorate']
+    required: [true, "Please provide your residence governorate"],
   },
   academicBackground: {
     type: String,
-    required: [true, 'Please provide a study level']
+    required: [true, "Please provide a study level"],
   },
   interestedFields: {
     type: Array,
-    required: [true, 'Please choose at least one field you are interested in']
+    required: [true, "Please choose at least one field you are interested in"],
   },
   major: {
     type: String,
-    required: [true, 'Please tell us your major/focus area']
+    required: [true, "Please tell us your major/focus area"],
   },
   participatingReason: {
     type: String,
     required: [
       true,
-      'Please tell us reason for participating in AppX 20 Hackathon'
-    ]
+      "Please tell us reason for participating in AppX 20 Hackathon",
+    ],
   },
   haveTeam: {
     type: String,
-    required: [true, 'Please tell us if you have a team']
+    required: [true, "Please tell us if you have a team"],
   },
   teamName: {
-    type: String
+    type: String,
   },
 
   comments: {
-    type: String
+    type: String,
   },
   hearAboutUs: {
-    type: String,
-    required: [true, 'Please tell us how you hear about us']
-  }
+    type: Array,
+    required: [true, "Please tell us how you hear about us"],
+  },
 });
 
-const HackathonUser = mongoose.model('HackathonUser', hackathonUserSchema);
+const HackathonUser = mongoose.model("HackathonUser", hackathonUserSchema);
 
 module.exports = HackathonUser;
--- models/summitUserModel.js
+++ models/summitUserModel.js
@@ -1,43 +1,43 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const summitUserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name'],
-    trim: true
+    required: [true, "Please tell us your name"],
+    trim: true,
   },
   email: {
     type: String,
-    required: [true, 'Please tell us your email'],
-    unique: [true, 'Your email is used, please register with another email'],
-    validate: [validator.isEmail, 'Please provide a vaild email']
+    required: [true, "Please tell us your email"],
+    unique: [true, "Your email is used, please register with another email"],
+    validate: [validator.isEmail, "Please provide a vaild email"],
   },
   nationalID: {
     type: Number,
     unique: true,
-    required: [true, 'Please tell us your national ID']
+    required: [true, "Please tell us your national ID"],
   },
   phone: {
-    type: Number
+    type: Number,
   },
   governorate: {
     type: String,
-    required: [true, 'Please provide your residence governorate']
+    required: [true, "Please provide your residence governorate"],
   },
   academicBackground: {
     type: String,
-    required: [true, 'Please provide a study level']
+    required: [true, "Please provide a study level"],
   },
   comments: {
-    type: String
+    type: String,
   },
   hearAboutUs: {
-    type: String,
-    required: [true, 'Please tell us how you hear about us']
-  }
+    type: Array,
+    required: [true, "Please tell us how you hear about us"],
+  },
 });
 
-const SummitUser = mongoose.model('SummitUser', summitUserSchema);
+const SummitUser = mongoose.model("SummitUser", summitUserSchema);
 
 module.exports = SummitUser;
