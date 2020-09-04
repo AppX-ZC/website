350a6a4f9f846142fdf4024918a7a8876e7180b8
ahmedsamir-dev <ahmedsamirwarda22@gmail.com>
2020-09-04 19:17:13 +0200

    implement cors

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
-const formRouter = require('./routes/formRouter');
-const userRouter = require('./routes/userRouter');
-const postRouter = require('./routes/postRouter');
+const formRouter = require("./routes/formRouter");
+const userRouter = require("./routes/userRouter");
+const postRouter = require("./routes/postRouter");
 
-const contactUs = require('./controllers/contactController');
-const globalErrorHandler = require('./controllers/errorController');
-const authController = require('./controllers/authController');
+const contactUs = require("./controllers/contactController");
+const globalErrorHandler = require("./controllers/errorController");
+const authController = require("./controllers/authController");
 
 const app = express();
 
-app.enable('trust proxy');
+app.enable("trust proxy");
+
+app.use(cors());
 
 app.use(helmet());
 
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 //Data Santization against NoSQL query injection
 app.use(mongoSanitize());
@@ -28,13 +31,13 @@ app.use(xss());
 
 app.use(express.json());
 
-app.post('/login', authController.login);
-app.get('/logout', authController.logout);
+app.post("/login", authController.login);
+app.get("/logout", authController.logout);
 
-app.post('/contact-us', contactUs);
-app.use('/form', formRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/posts', postRouter);
+app.post("/contact-us", contactUs);
+app.use("/form", formRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 app.use(globalErrorHandler);
 module.exports = app;
